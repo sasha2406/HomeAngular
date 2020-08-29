7da3626ba003123e1271dfea8e3378bc955d0383
sasha2406 <sasha24065@gmail.com>
2020-08-29 22:16:21 +0300

    first component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from "@ngrx/store";
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
